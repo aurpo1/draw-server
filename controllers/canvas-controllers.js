3b00e04796553f30f7682d537d64e5c5a735b29c
yeseo.bang <yeseo.bang@lge.com>
2024-03-08 10:59:25 +0900

    test

--- controllers/canvas-controllers.js
+++ controllers/canvas-controllers.js
@@ -1,25 +1,33 @@
 const HttpError = require('../models/http-error');
 
 let exportImg = {
-  id: '',
-  base: ''
-}
+  'test': '',
+};
 
 const getImgById = (req, res, next) => {
-  if (!exportImg.id) {
+  console.log(req.params.cid);
+  console.log(exportImg[req.params.cid]);
+  if (!exportImg[req.params.cid]) {
     throw new HttpError('Cannot found canvas image', 404);
   }
-  res.send(`<img src="${exportImg.base}"/>`);
+  console.log(req);
+  console.log('--------------------');
+  console.log(res);
+  res.send(`<img src="${exportImg[req.params.cid]}"/>`);
 }
 
 const createImg = (req, res, next) => {
   const { id, base } = req.body;
-  const newCanvas = {
-    id,
-    base
-  }
-  exportImg = newCanvas;
-  res.status(201).json({canvas: newCanvas});
+
+  exportImg[id] = base;
+  // setTimeout(() => {
+  //   delete exportImg[id];
+  //   console.log('delete ', exportImg);
+  // }, 20000);
+  console.log(req);
+  console.log('--------------------');
+  console.log(res);
+  res.status(201).json({msg: 'Succeed to save'});
 }
 
 exports.getImgById = getImgById;
